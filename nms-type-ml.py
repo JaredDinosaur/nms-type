c0fe15b757afbc8f906cc2d420ede95e72233005
Jared Dinosaur <61011316+JaredDinosaur@users.noreply.github.com>
2025-06-19 13:15:48 +0100

    Add files via upload

--- nms-type-ml.py
+++ nms-type-ml.py
@@ -7,11 +7,11 @@ file=open("nms.tmp","r")
 linecount=0
 for x in file.readlines():
     linecount=linecount+1
-    if linecount == 1:
+    if linecount % maxlc == 1:
         file2=open("nms2.tmp","w")
         file2.write(x)
     elif linecount % maxlc == 0: 
-        file2=open("nms2.tmp","w")
+        file2=open("nms2.tmp","a")
         file2.write(x)
         os.system("cat nms2.tmp | nms -a")
     else:
