5cf43b9214f7f0e79dbd2ce0b8a25ee27a966439
Jared Dinosaur <61011316+JaredDinosaur@users.noreply.github.com>
2025-07-15 22:07:19 +0100

    Add files via upload

--- nms-install.py
+++ nms-install.py
@@ -1,12 +1,18 @@
 import os
 print("This script must be run as root or a user with sudo privileges.")
 print("This script requires the packages git, gcc and make.")
-yay=input("Do you use yay as your package manager? (y/N) ")
-if yay.lower()=="y":
-    os.system("yay -S no-more-secrets")
-else:
-    os.system("git clone https://github.com/bartobri/no-more-secrets.git")
-    os.system("cd no-more-secrets")
-    os.system("make nms")
-    os.system("sudo make install")
-print("Script complete!")
+ready=input("Enter 'ready' to run the script, or anything else to cancel: ")
+if ready.lower()=="ready":
+    os.system("clear")
+    print("Checking if yay is installed...")
+    os.system("yay -h > check.yay")
+    file=open("check.yay","r")
+    if file.read() == "":
+        print("yay not found. Using standard installation methods...")
+        time.sleep(2)
+        os.system("git clone https://github.com/bartobri/no-more-secrets.git")
+        os.system("cd no-more-secrets && make nms && sudo make install")
+    else:
+        print("Installing packages using yay...")
+        os.system("yay -S no-more-secrets")
+    print("Script complete!")
